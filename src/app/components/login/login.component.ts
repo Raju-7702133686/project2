f2c6d4695602d2d6a06f6bacffd6fdec83087feb
Raju <rajuprasad7919@gmail.com>
2024-07-08 18:34:11 +0530

    Login reactive implementation completed

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FirstValueFromConfig } from 'rxjs/internal/firstValueFrom';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  public userForm: FormGroup = new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required, Validators.minLength(8),Validators.maxLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+    ])
+  })
+
+  login(){
+    console.log(this.userForm);
+  }
 
 }
